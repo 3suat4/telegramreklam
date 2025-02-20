8b4184aa38567ead36b293d8949a67d523e862fe
3suat4 <97811421+3suat4@users.noreply.github.com>
2025-02-20 05:41:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     console.log("Script çalıştı");
 
     var user_id = window.location.hash.substring(1);
@@ -16,29 +16,17 @@ document.addEventListener("DOMContentLoaded", function() {
         var seconds = 20;
         timerElement.innerText = "Geri Sayım: " + seconds + " saniye";
 
-        var interval = setInterval(function() {
+        var interval = setInterval(function () {
             seconds--;
             timerElement.innerText = "Geri Sayım: " + seconds + " saniye";
 
             if (seconds <= 0) {
                 clearInterval(interval);
-                var serverUrl = "http://34.116.169.108:5000/#"; 
                 
-                fetch(serverUrl + encodeURIComponent(user_id))
-                    .then(response => response.text()) 
-                    .then(data => {
-                        console.log("Sunucudan gelen yanıt:", data);
-                        try {
-                            let jsonData = JSON.parse(data);
-                            document.body.innerHTML = "<h2>" + jsonData.message + "</h2>";
-                        } catch (e) {
-                            document.body.innerHTML = "<h2>Geçersiz yanıt: " + data + "</h2>";
-                        }
-                    })
-                    .catch(error => {
-                        console.error("Ödül tetikleme hatası:", error);
-                        document.body.innerHTML = "<h2>Sunucuya bağlanılamadı!</h2>";
-                    });
+                // Kullanıcıyı yönlendir
+                var redirectUrl = "http://34.116.169.108:5000/#" + encodeURIComponent(user_id);
+                console.log("Yönlendirme yapılıyor: " + redirectUrl);
+                window.location.href = redirectUrl;
             }
         }, 1000);
     } else {
