6131c42207d83c2548bed7481da08fc2aada1f4e
3suat4 <97811421+3suat4@users.noreply.github.com>
2025-03-06 04:36:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Script çalıştı");
 
-  var user_id = window.location.hash.substring(1);
-
-  // Eğer hash mevcutsa ve yönlendirilmemişse geri sayımı başlat
-  if (user_id && !sessionStorage.getItem("redirected")) {
-    var seconds = 0;
-    var timerElement = document.getElementById("timer");
-
-    if (!timerElement) {
-      timerElement = document.createElement("div");
-      timerElement.id = "timer";
-      timerElement.style.fontSize = "24px";
-      timerElement.style.marginTop = "20px";
-      document.body.appendChild(timerElement);
-    }
-
-    timerElement.innerText = "Geri Sayım: " + seconds + " saniye";
-
-    var interval = setInterval(function () {
-      seconds--;
-      timerElement.innerText = "Geri Sayım: " + seconds + " saniye";
-
-      if (seconds <= 0) {
-        clearInterval(interval);
-        // Süre bitince geri sayım mesajı gizlenir ve "Ödül Al" butonu görünür hale gelir.
-        timerElement.style.display = "none";
-        var rewardButton = document.getElementById("rewardButton");
-        if (rewardButton) {
-          rewardButton.style.display = "inline-block";
-        }
-      }
-    }, 1000);
-  } else if (sessionStorage.getItem("redirected")) {
-    document.body.innerHTML = "<h2>Yönlendirildiniz.</h2>";
-  } else {
-    document.body.innerHTML = "<h2>Kullanıcı ID'si eksik.</h2>";
-  }
-
-  // Ödül Al butonu için tıklama olayı
   var rewardButton = document.getElementById("rewardButton");
-  if (rewardButton) {
-    rewardButton.addEventListener("click", function () {
-      if (user_id) {
-        redirectToReward(user_id);
-      } else {
-        alert("Ödül almak için kullanıcı ID'si bulunamadı!");
-      }
-    });
-  }
+  var userIdInput = document.getElementById("userIdInput");
+
+  rewardButton.addEventListener("click", function () {
+    var user_id = userIdInput.value.trim();
+    if (user_id) {
+      redirectToReward(user_id);
+    } else {
+      alert("Lütfen kullanıcı ID'sini giriniz.");
+    }
+  });
 });
 
-// Kullanıcıyı yönlendiren fonksiyon
+// Kullanıcıyı ödül sitesine yönlendiren fonksiyon
 function redirectToReward(user_id) {
   var redirectUrl = "http://34.116.169.108:5000/#" + encodeURIComponent(user_id);
   console.log("Yönlendirme yapılıyor: " + redirectUrl);
-  sessionStorage.setItem("redirected", "true");
   window.location.href = redirectUrl;
 }
